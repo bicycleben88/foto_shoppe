8adf5ba28ae79de7608ecf2ed13af836e2a97c4c
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2020-12-26 09:37:08 -0500

    add picture w / useMutation

--- pages/api/pictures/create.js
+++ pages/api/pictures/create.js
@@ -2,33 +2,33 @@ import { PrismaClient } from "@prisma/client";
 
 export default async (req, res) => {
   const prisma = new PrismaClient({ log: ["query"] });
-  const {
-    query: { id },
-  } = req;
+  // const {
+  //   query: { id },
+  // } = req;
 
-  res.json({ id: id });
-  // try {
-  //   const { name, description, image, albumId } = req.body;
-  //   const albumWithPictue = await prisma.album.update({
-  //     where: {
-  //       id: albumId,
-  //     },
-  //     data: {
-  //       pictures: {
-  //         create: {
-  //           name: name,
-  //           description: description,
-  //           image: image,
-  //         },
-  //       },
-  //     },
-  //   });
-  //   res.statusCode = 201;
-  //   res.json({ albumWithPictue });
-  // } catch (error) {
-  //   res.statusCode = 500;
-  //   res.json({ error: error, picture: req.body });
-  // } finally {
-  //   prisma.$disconnect();
-  // }
+  // res.json({ id: id });
+  try {
+    const { name, description, image, albumId } = req.body;
+    const albumWithPictue = await prisma.album.update({
+      where: {
+        id: albumId,
+      },
+      data: {
+        pictures: {
+          create: {
+            name: name,
+            description: description,
+            image: image,
+          },
+        },
+      },
+    });
+    res.statusCode = 201;
+    res.json({ albumWithPictue });
+  } catch (error) {
+    res.statusCode = 500;
+    res.json({ error: error, picture: req.body });
+  } finally {
+    prisma.$disconnect();
+  }
 };
