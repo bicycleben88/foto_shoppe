39f7ddd11bbcc297b684b2a012ef115080a6e980
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2020-12-29 18:45:08 -0500

    move dependencies .json

--- pages/albums/[id]/index.js
+++ pages/albums/[id]/index.js
@@ -29,25 +29,7 @@ async function deletePicture(pictureId) {
   });
 }
 
-// export async function getStaticPaths() {
-//   const albums = await queryAlbums();
-//   const paths = [];
-
-//   albums.map((album) => {
-//     return paths.push({
-//       params: {
-//         id: album.id.toString(),
-//       },
-//     });
-//   });
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-
 export async function getServerSideProps(context) {
-  console.log(context.query.id);
   const response = await queryAlbum(context.query.id);
   const data = await JSON.stringify(response);
   const initialAlbumData = await JSON.parse(data);
