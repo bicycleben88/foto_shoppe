8dd95d58c8c4a67e02ae17e29611b948060f040d
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2021-01-14 18:45:55 -0500

    useSession create album

--- pages/api/albums/create.js
+++ pages/api/albums/create.js
@@ -1,22 +1,29 @@
 import { PrismaClient } from "@prisma/client";
+import { getSession } from "next-auth/client";
 
 export default async (req, res) => {
-  const prisma = new PrismaClient({ log: ["query"] });
+  const session = await getSession({ req });
+  if (session) {
+    const prisma = new PrismaClient({ log: ["query"] });
 
-  try {
-    const { name, description } = req.body;
-    const album = await prisma.album.create({
-      data: {
-        name: name,
-        description: description,
-      },
-    });
-    res.statusCode = 201;
-    res.json({ album });
-  } catch (error) {
-    res.statusCode = 500;
-    res.json({ error: "Couldn't Create Album" });
-  } finally {
-    await prisma.$disconnect();
+    try {
+      const { name, description } = req.body;
+      const album = await prisma.album.create({
+        data: {
+          name: name,
+          description: description,
+        },
+      });
+      res.statusCode = 201;
+      res.json({ album });
+    } catch (error) {
+      res.statusCode = 500;
+      res.json({ error: "Couldn't Create Album" });
+    } finally {
+      await prisma.$disconnect();
+    }
+  } else {
+    res.statusCode = 401;
+    res.json({ error: "You're not Logged In" });
   }
 };
