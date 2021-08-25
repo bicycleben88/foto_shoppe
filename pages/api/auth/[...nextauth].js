9441a963777f4c4e46dc1e9f603b00849e9c8019
Ben Higginbotham <bicycleben88@gmail.com>
2021-08-25 11:17:15 -0400

    add oAuth options to object

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,28 +5,31 @@ import Providers from "next-auth/providers";
 
 const prisma = new PrismaClient();
 
-export default async (req, res) => {
-  let clientId =
-    process.env.NODE_ENV === "development"
-      ? process.env.GIT_DEVELOPMENT_ID
-      : process.env.GIT_PRODUCTION_ID;
-  let clientSecret =
-    process.env.NODE_ENV === "development"
-      ? process.env.GIT_DEVELOPMENT_SECRET
-      : process.env.GIT_PRODUCTON_SECRET;
+let clientId =
+  process.env.NODE_ENV === "development"
+    ? process.env.GIT_DEVELOPMENT_ID
+    : process.env.GIT_PRODUCTION_ID;
+let clientSecret =
+  process.env.NODE_ENV === "development"
+    ? process.env.GIT_DEVELOPMENT_SECRET
+    : process.env.GIT_PRODUCTON_SECRET;
 
-  await NextAuth(req, res, {
-    providers: [
-      Providers.GitHub({
-        clientId: clientId,
-        clientSecret: clientSecret,
-      }),
-    ],
-    debug: process.env.NODE_ENV === "development",
-    secret: process.env.AUTH_SECRET,
-    jwt: {
-      secret: process.env.JWT_SECRET,
-    },
-    adapter: Adapters.Prisma.Adapter({ prisma }),
-  });
+const options = {
+  providers: [
+    Providers.GitHub({
+      clientId: clientId,
+      clientSecret: clientSecret,
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  secret: process.env.AUTH_SECRET,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+};
+
+export default (req, res) => {
+  console.log(options.providers);
+  return NextAuth(req, res, options);
 };
