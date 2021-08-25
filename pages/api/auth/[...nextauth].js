6c063df40e4d5b9cae682767a1fd7dfb0bd7a78d
Ben Higginbotham <bicycleben88@gmail.com>
2021-08-25 10:45:16 -0400

    save oAuth keys to variables

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,24 +5,28 @@ import Providers from "next-auth/providers";
 
 const prisma = new PrismaClient();
 
-export default (req, res) =>
-  NextAuth(req, res, {
+export default async (req, res) => {
+  let clientId =
+    process.env.NODE_ENV === "development"
+      ? process.env.GIT_DEVELOPMENT_ID
+      : process.env.GIT_PRODUCTION_ID;
+  let clientSecret =
+    process.env.NODE_ENV === "development"
+      ? process.env.GIT_DEVELOPMENT_SECRET
+      : process.env.GIT_PRODUCTON_SECRET;
+
+  await NextAuth(req, res, {
     providers: [
       Providers.GitHub({
-        clientId:
-          process.env.NODE_ENV === "development"
-            ? process.env.GIT_DEVELOPMENT_ID
-            : process.env.GIT_PRODUCTION_ID,
-        clientSecret:
-          process.env.NODE_ENV === "development"
-            ? process.env.GIT_DEVELOPMENT_SECRET
-            : process.env.GIT_PRODUCTON_SECRET,
+        clientId: clientId,
+        clientSecret: clientSecret,
       }),
     ],
-    debug: process.env.NODE_ENV === "development",
+    debug: process.env.NODE_ENV === "develoçpment",
     secret: process.env.AUTH_SECRET,
     jwt: {
       secret: process.env.JWT_SECRET,
     },
     adapter: Adapters.Prisma.Adapter({ prisma }),
   });
+};
